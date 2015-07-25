e6d94b63e97c22bb8bfb7d7903ca35fd839e2b47
Sherry Hua <Sherry_Hua@epam.com>
2015-07-25 13:48:54 +0800

    account

--- app/web/modules/account/00-config.js
+++ app/web/modules/account/00-config.js
@@ -11,4 +11,9 @@ angular.module('webapp.account', [])
 		templateUrl : 'web/modules/account/member_myacc_01.html',
 		controller: 'IndexCtrl'
 	});
+    $stateProvider.state('app.member_myacc_02', {
+        url : 'member_myacc_02',
+        templateUrl : 'web/modules/account/member_myacc_02.html',
+        controller: 'IndexCtrl'
+    });
 });
