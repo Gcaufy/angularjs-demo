f5a2b9c32c88f0685a3f5659ba97c039478c59b0
Gcaufy <gcaufy@gmail.com>
2015-09-01 15:33:44 +0800

    fixed bug of gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,8 @@
+var extend = require('util')._extend;
+var config = extend(
+    require('./config/config').APPCONFIG,
+    require('./config/config-local').APPCONFIG);
+
 var gulp = require('gulp');
 	crypto = require('crypto');
 	jshint = require('gulp-jshint'),
@@ -12,19 +17,24 @@ var gulp = require('gulp');
     colors = require('colors'),
     spritesmith = require('gulp.spritesmith'),
     mergeStream = require('merge-stream'),
+    gulpSequence = require("gulp-sequence"),
     uglify = require('gulp-uglify');
 	//ver = require('gulp-ver'),
 	//rimraf = require('gulp-rimraf'),
 	//changed = require('gulp-changed'),
     //symlink = require('gulp-symlink'),
 	//gulpFilter = require('gulp-filter'),
-    //browserSync = require('browser-sync').create(),
-    //livereload = require('gulp-livereload'),
+
+
 
 var map = require('map-stream');
 
 var lang = 'en_us';
 var hash = '';
+
+var currentDate = new Date();
+var versionTag = '' + currentDate.getFullYear() + pad(currentDate.getMonth() + 1, 2) + pad(currentDate.getDate(), 2) + '-'
+                + pad(currentDate.getHours(), 2) + pad(currentDate.getMinutes(), 2) + pad(currentDate.getSeconds(), 2);
 
 var flagJs = false, flagCss = false, flagHtml = false, flagImg = false;
 
@@ -37,7 +47,11 @@ var options = {
             '!app/vendor/**/*.js',
             '!app/assets/js/all.*.js',
             'app/web/index.js',
-            'config/*.js',
+            'config/config.js',
+            'config/config-local.js',
+            'app/_source/common/**/*.js',
+            'app/_source/i18n/**/*.js',
+            'config/**/*.js',
             'app/web/**/*.js'
         ]
     },
@@ -80,7 +94,9 @@ function errorReporter () {
         cb(null, file);
     });
 };
-
+function pad(num, n) {
+    return (Array(n).join(0) + num).slice(-n); 
+}
 function getHash() {
     var random = (new Date()).toISOString().slice(0, 10).replace(/-/g, "") + (new Date()).toISOString().slice(11, 16).replace(/:/g, "") + Math.random().toString().substr(0,8);
     var hasher = crypto.createHash('sha1');
@@ -102,7 +118,7 @@ gulp.task('bower', function() {
     return bower(options.vendor).pipe(gulp.dest(options.vendor));
 });
 gulp.task('githooks', function () {
-    return gulp.src(options.hooks).pipe(chmod(755)).pipe(gulp.dest('.git/hooks/')); 
+    return gulp.src(options.hooks).pipe(chmod(755)).pipe(gulp.dest('.git/hooks/'));
 });
 gulp.task('jshint', function() {
   	return gulp.src(options.js.src)
@@ -121,7 +137,6 @@ gulp.task('jshint', function() {
 
 gulp.task('css', ['img_build'], function () {
     flagCss = true;
-
     var stream = gulp.src(options.css.compile)
     .pipe(sass({includePaths: options.css.include}).on('error', sass.logError))
     .pipe(rename(options.css.name));
@@ -139,16 +154,16 @@ gulp.task('js', function () {
     del(options.js.clean);
   	return stream.pipe(gulp.dest(options.js.dest));
 });
- 
+
 gulp.task('img_build', function(cb) {
     if (flagImg === true) {
         flagImg = false;
-        var spriteData = 
+        var spriteData =
             gulp.src(options.img.src) // source path of the sprite images
                 .pipe(spritesmith({
                     imgName: options.img.spriteImageName,
                     cssName: options.img.spriteCssName,
-                    cssTemplate: '.scss.template.handlebars',
+                    cssTemplate: '.scss.template.handlebars'
             }));
         del(options.img.clean);
         spriteData.img.pipe(gulp.dest(options.img.imgDest)); // output path for the sprite
@@ -169,6 +184,7 @@ gulp.task('img', function () {
 
 gulp.task('gen', function(){
     var stream = gulp.src(options.html.index);
+
     if (flagJs) {
         stream = stream.pipe(replace(/all\..*\.js/g, 'all' + '.' + hash + '.js'));
         flagJs = false;
@@ -178,6 +194,10 @@ gulp.task('gen', function(){
         flagCss = false;
     }
     if (flagHtml) {
+        stream = stream.pipe(replace(/\r?\n\s*<script.*version=\".*/, ''));
+        if (config.SHOW_VERSION) {
+            stream = stream.pipe(replace(/\<!--\sSHOW\sVERSION\s-->/, '<!-- SHOW VERSION -->\n        <script src="assets/js/versionmark.js" version="' + versionTag + '"></script>'));
+        }
         stream = stream.pipe(replace(/window\.BUILD_HTML_HASH\s\=\s\'.*'/g, "window.BUILD_HTML_HASH = '" + hash + "'"));
         flagHtml = false;
     }
@@ -192,28 +212,37 @@ gulp.task('minify', function () {
 });
 
 
-gulp.task('default', ['img', 'css', 'js', 'html', 'gen']);
+gulp.task('default',function(cb) {
+    gulpSequence('img','css','js','html','gen')(cb)
+});
 
 var fileChange = function (e) {
     updateHash();
     console.log(e.path + ' was ' + e.type + ', running task...');
 }
+gulp.task('onlyChangeJS',function(cb) {
+    gulpSequence('js', 'gen')(cb)
+});
+
+gulp.task('onlyChangeCSS',function(cb) {
+    gulpSequence('css', 'gen')(cb)
+});
+gulp.task('onlyChangeHTML',function(cb) {
+    gulpSequence('html', 'gen')(cb)
+});
+gulp.task('onlyChangeIMG',function(cb) {
+    gulpSequence('img','css', 'gen')(cb)
+});
+
 
 gulp.task('watch', function (cb) {
-    //livereload.listen();
-    /*gulp.watch(options.js.src, ['js', 'gen']).on('change', fileChange);
-    gulp.watch(options.css.watch, ['css','gen']).on('change', fileChange);
-    gulp.watch(options.html.watch, ['html','gen']).on('change', fileChange);
-    gulp.watch(options.img.src, ['img']).on('change', fileChange);
-    gulp.watch('gulpfile.js', ['default']).on('change', fileChange);*/
-
-    gulp.watch(options.js.src, ['default']).on('change', fileChange);
-    gulp.watch(options.css.watch, ['default']).on('change', fileChange);
-    gulp.watch(options.html.watch, ['default']).on('change', fileChange);
-    gulp.watch(options.img.src, ['default']).on('change', fileChange);
-    gulp.watch('gulpfile.js', ['default']).on('change', fileChange);
+    gulp.watch(options.js.src, ['onlyChangeJS']).on('change', fileChange);
+    gulp.watch(options.css.watch, ['onlyChangeCSS']).on('change', fileChange);
+    gulp.watch(options.html.watch, ['onlyChangeHTML']).on('change', fileChange);
+    gulp.watch(options.img.src, ['onlyChangeIMG']).on('change', fileChange);
+    gulp.watch('gulpfile.js', ['onlyChangeJS']).on('change', fileChange);
     cb();
-}); 
+});
 
 
 gulp.task('connect', function(cb) {
